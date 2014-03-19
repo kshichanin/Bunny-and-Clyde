568770464229fd119467f76008d5ff16bf1d8d3a
mason0 <mlp4sf@virginia.edu>
2014-03-19 16:32:07 -0400

    added sprite class

---
+++ Bunny-and-Clyde/Bunny-and-Clyde/Sprite.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+
+namespace Bunny_and_Clyde
+{
+    class Sprite
+    {
+        public int X
+        {
+            get;
+            set;
+        }
+
+        public int Y
+        {
+            get;
+            set;
+        }
+
+    }
+}
