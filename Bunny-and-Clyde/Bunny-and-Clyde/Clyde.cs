1283b5a4645cf4a8def1f7c312e7704dc636a965
Casey Silver <caseyleesilver@gmail.com>
2014-03-20 18:33:09 -0400

    Adding Clyde Sprite class (for real hopefully)

---
+++ Bunny-and-Clyde/Bunny-and-Clyde/Clyde.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bunny_and_Clyde
+{
+    class Clyde : Sprite
+    {
+        static int CLYDE_WIDTH = 58, CLYDE_HEIGHT = 48;
+
+        public float Velocity { get; set; }
+        public enum State
+        {
+            Default,
+            Airbourne,
+            Swimming,
+            Diving
+        }
+        public State ClydeState { get; set; }
+
+        public Clyde(float initialX, float initialY)
+            : base("Turtle", initialX, initialY, CLYDE_WIDTH, CLYDE_HEIGHT)
+        {
+            Velocity = 0;
+            ClydeState = State.Default;
+        }
+
+    }
+}
