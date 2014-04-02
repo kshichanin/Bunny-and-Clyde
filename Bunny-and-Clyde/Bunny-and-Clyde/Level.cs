944a8881eab83096ca8e23777c650be9b564783b
SimonSchama <ejl3tf@virginia.edu>
2014-04-02 17:51:23 -0400

    switches wrok

--- Bunny-and-Clyde/Bunny-and-Clyde/Level.cs
+++ Bunny-and-Clyde/Bunny-and-Clyde/Level.cs
@@ -110,13 +110,13 @@ namespace Bunny_and_Clyde
                     Color c = new Color (drawColor .R ,drawColor .G ,drawColor .B ,drawColor .A);
                     currentObject = new Door(this, c, o.X, o.Y, o.Width, o.Height);
                 }
-                else if (o.Properties["type"] == "switch_block")
+                else if (o.Properties["type"] == "switch_button")
                 {
                     System.Drawing.Color drawColor = System.Drawing.Color.FromName(o.Properties["color"]);
                     Color c = new Color(drawColor.R, drawColor.G, drawColor.B, drawColor.A);
                     currentObject = new Switch(c,this, o.X, o.Y, o.Width, o.Height);
                 }
-                else if (o.Properties["type"] == "switch_door")
+                else if (o.Properties["type"] == "switch_gate")
                 {
                     System.Drawing.Color drawColor = System.Drawing.Color.FromName(o.Properties["color"]);
                     Color c = new Color(drawColor.R, drawColor.G, drawColor.B, drawColor.A);
