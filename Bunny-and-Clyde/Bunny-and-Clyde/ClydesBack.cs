84b57eb6ee2e7dce430511a45089a03a7967f494
mason0 <mlp4sf@virginia.edu>
2014-04-03 20:51:03 -0400

    Added level 3

--- Bunny-and-Clyde/Bunny-and-Clyde/ClydesBack.cs
+++ Bunny-and-Clyde/Bunny-and-Clyde/ClydesBack.cs
@@ -8,7 +8,7 @@ namespace Bunny_and_Clyde
     class ClydesBack : Item 
     {
         public ClydesBack(float x, float y, int width, int height)
-            : base("stone_block_4x1", x, y, width, height)
+            : base("blank.png", x, y, width, height)
         {
 
         }
